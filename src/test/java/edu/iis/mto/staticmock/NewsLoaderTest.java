97090d72c03cee0309dab7dd71a7c6cdab15c6ad
MichalakJ <daran.dasaner@gmail.com>
2016-04-13 20:21:59 +0200

    created base for testing

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.iis.mto.staticmock;
+
+import edu.iis.mto.staticmock.reader.NewsReader;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.mockito.Matchers.any;
+/**
+ *
+ * @author Kuba
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { ConfigurationLoader.class, NewsReaderFactory.class } )
+public class NewsLoaderTest {
+    ConfigurationLoader configLoader;
+    NewsReaderFactory newsReaderFactory;
+    Configuration config;
+    NewsReader newsReader;
+    @Before
+    public void setUpForTest() throws Exception {
+        mockStatic(ConfigurationLoader.class);
+	mockStatic(NewsReaderFactory.class);
+        
+        configLoader = mock(ConfigurationLoader.class);
+	newsReaderFactory = mock(NewsReaderFactory.class);
+	config = mock(Configuration.class);
+	newsReader = mock(NewsReader.class);
+
+	when(ConfigurationLoader.getInstance()).thenReturn(configLoader);
+	when(configLoader.loadConfiguration()).thenReturn(config); 
+        when(NewsReaderFactory.getReader(any(String.class))).thenReturn(newsReader);
+                
+        }
+    
+    @Test
+    public void givenTwoNewsWithNoSubsciption_whenLoadNews_addPublicInfoCalledTwoTimes(){
+        IncomingNews news = new IncomingNews();
+        news.add(new IncomingInfo("asd", SubsciptionType.NONE));
+        news.add(new IncomingInfo("bles", SubsciptionType.NONE));
+        when(newsReader.read()).thenReturn(news);
+        NewsLoader newsLoader = new NewsLoader();
+        PublishableNews result = newsLoader.loadNews();
+   
+        
+    }
+}
